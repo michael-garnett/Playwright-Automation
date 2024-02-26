38cd1ea58a3c73fb2475383e7e7fbd90cf113d69
michael-garnett <garnett.michael79@gmail.com>
2024-02-26 03:51:11 -0500

    SS + date

--- tests/Screenshots.spec.js
+++ tests/Screenshots.spec.js
@@ -1,8 +1,28 @@
 import {test, expect} from '@playwright/test'
 
+// Get the current date object
+const currentDate = new Date();
+
+// Format the date as YYYY-MM-DD (ISO 8601 format) for filename compatibility
+const formattedDate = currentDate.toISOString().slice(0, 10);
+
 test('page screenshot', async ({page}) =>{
 
     await page.goto('https://testautomationpractice.blogspot.com/')
-    await page.screenshot({ path: 'test-results\screenshots' + Date.now + 'LandingPage.png' })
+    await page.screenshot({ path: 'test-results/Screenshots/' + formattedDate + 'LandingPage.png' })
+
+})
+
+test('full page screenshot', async ({page}) =>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    await page.screenshot({ path: 'test-results/Screenshots/' + formattedDate + 'FullLandingPage.png', fullPage:true })
+
+})
+
+test.only('locator screenshot', async ({page}) =>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    await page.locator("#productTable").screenshot({ path: 'test-results/Screenshots/' + formattedDate + 'Locator.png', fullPage:true })
 
 })
